512afcdcabf7749d24978d0fb4b9bb5af7a67c1f
lucasalegre00 <lucasmaxialegre@gmail.com>
2024-07-20 19:16:55 -0300

    trabajando con arrgelos

---
+++ Arreglo/D2.java
@@ -0,0 +1,13 @@
+package Arreglo;
+
+public class D2 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+                for(int i=1; i<arr.length-1;i++){
+                    System.out.println(arr[i]);
+
+        }
+
+
+    }
+}
